4f0dd5af0f0b7c1102a2d33934c56a1c41216e10
raghu.avula93@hotmail.com <raghu.avula93@hotmail.com>
2014-11-20 00:02:33 -0500

    original works

--- modified_code/IG.R
+++ modified_code/IG.R
@@ -6,11 +6,10 @@ source("/Users/Raghu/Documents/asthma2/modified_code/dir_info.R")
 source(libfile1)
 #source(libfile2)
 
-## get parameters ##
-resdir=file.path(res.dir,"original") #modified to original
+## get parameters #
 
 # read clinical data #
-clinic.file=file.path(kmeans.newdir, resdir, "datacluster_6clpats_NOalvmacro5neword.txt")
+clinic.file=file.path(data.dir, "dataset.txt")
 clinic.data=read.delim(clinic.file)
 data=apply(clinic.data[, 4:ncol(clinic.data)], 2, as.numeric)
 col.data=colnames(data)
@@ -34,8 +33,8 @@ n.class.pheno=length(unique(phenotypes))
       data1=data[,i.var]
     }
 
-    infogain.data=c(infogain.data, formatdata(infogain(phenotypes, data1)))
-    su.data=c(su.data, formatdata(su(phenotypes, data1)))
+    infogain.data=c(infogain.data, infogain(phenotypes, data1))
+    su.data=c(su.data, infogain.normalized(phenotypes, data1))
     discret.data=rbind(discret.data, data1) 
   }
 
@@ -43,22 +42,19 @@ n.class.pheno=length(unique(phenotypes))
   idx.varig.reord=idx.varig[order(infogain.data[idx.varig], decreasing=T)]
   infogain.data.all=cbind(col.data[idx.varig.reord], infogain.data[idx.varig.reord])
   colnames(infogain.data.all)=c("Variable", "InfoGain")
-  infogain.file=gsub("datacluster", "InfoGainEW", basename(clinic.file))
-  infogain.file.fp=file.path(kmeansclass.newdir, infogain.file)
+  infogain.file.fp=file.path(res.dir, 'InfoGain.txt')
   write.table(infogain.data.all, infogain.file.fp, row.names=F,col.names=T, quote=F, sep="\t")
 
   idx.varsu=1:ncol(data)
   idx.varsu.reord=idx.varsu[order(su.data[idx.varsu], decreasing=T)]
   su.data.all=cbind(col.data[idx.varsu.reord], su.data[idx.varsu.reord])
   colnames(su.data.all)=c("Variable", "SU")
-  su.file=gsub("datacluster", "SuEW", basename(clinic.file))
-  su.file.fp=file.path(kmeansclass.newdir, su.file)
+  su.file.fp=file.path(res.dir,"SuEW.txt")
   write.table(su.data.all, su.file.fp, row.names=F,col.names=T, quote=F, sep="\t")
 
   discret.data.all=cbind(col.data[idx.varig.reord], discret.data[idx.varig.reord, ])
   colnames(discret.data.all)=c("Variable", phenotypes)
-  discret.data.file=gsub("datacluster", "discretEW", basename(clinic.file))
-  discret.data.file.fp=file.path(kmeansclass.newdir, discret.data.file)
+  discret.data.file.fp=file.path(res.dir, "discretEW.txt")
   write.table(discret.data.all, discret.data.file.fp, row.names=F,col.names=T, quote=F, sep="\t")
 
 
