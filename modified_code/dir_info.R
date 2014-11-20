c2490b8b72bbdcc0ce56b85541f66b8e26092466
raghu.avula93@hotmail.com <raghu.avula93@hotmail.com>
2014-11-20 00:15:42 -0500

    old code redo1

--- modified_code/dir_info.R
+++ modified_code/dir_info.R
@@ -4,6 +4,6 @@ code.dir="/Users/Raghu/Documents/asthma2/modified_code"
 
 data.dir="/Users/Raghu/Documents/asthma2/data"
 
-res.dir="/Users/Raghu/Documents/asthma2/results"
+res.dir="/Users/Raghu/Documents/asthma2/results/new/discret2/cluster%"
 
 libfile1=file.path(code.dir, "infogain_function.R")
