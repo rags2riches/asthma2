9130ad76f9e4cf1f50dbbcf386fe6fd81005674f
raghu.avula93@hotmail.com <raghu.avula93@hotmail.com>
2014-11-19 23:52:33 -0500

    first

---
+++ modified_code/dir_info.R
@@ -0,0 +1,9 @@
+base.dir="/Users/Raghu/Documents/asthma2"
+
+code.dir="/Users/Raghu/Documents/asthma2/modified_code"
+
+data.dir="/Users/Raghu/Documents/asthma2/data"
+
+res.dir="/Users/Raghu/Documents/asthma2/results"
+
+libfile1=file.path(code.dir, "infogain_function.R")
