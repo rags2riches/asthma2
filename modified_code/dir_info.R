cffa452c0bf0d8103e406ddde9bc28f8f10a1aef
raghu.avula93@hotmail.com <raghu.avula93@hotmail.com>
2014-11-20 12:25:52 -0500

    same

--- modified_code/dir_info.R
+++ modified_code/dir_info.R
@@ -4,6 +4,9 @@ code.dir="/Users/Raghu/Documents/asthma2/modified_code"
 
 data.dir="/Users/Raghu/Documents/asthma2/data"
 
-res.dir="/Users/Raghu/Documents/asthma2/results/new/discret2/cluster%"
+res.dir="/Users/Raghu/Documents/asthma2/results/new"
 
 libfile1=file.path(code.dir, "infogain_function.R")
+
+#"/Users/Raghu/Documents/asthma/modified_code/individual_infogain_function.R" 
+#code with bug in line 44 should be class(a) not class(s)
